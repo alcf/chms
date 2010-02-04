5f0f1a40a48365937aa542ae701d63ff5da1ea3b
Mike Ho <mike.ho@alcf.net>
2010-02-03 16:49:16 -0800

    Initial dat amodel design through first part of individual screens

---
+++ database/data.sql
@@ -0,0 +1,27 @@
+INSERT INTO role_type VALUES(1, 'Volunteer');
+INSERT INTO role_type VALUES(2, 'Staff Member');
+INSERT INTO role_type VALUES(3, 'ChMS Administrator');
+
+INSERT INTO membership_status_type VALUES(1, 'Non-Member');
+INSERT INTO membership_status_type VALUES(2, 'Member');
+INSERT INTO membership_status_type VALUES(3, 'Child of Member');
+INSERT INTO membership_status_type VALUES(4, 'Former Member');
+
+INSERT INTO permission_type VALUES(1, 'Edit Data');
+INSERT INTO permission_type VALUES(2, 'Access Stewardship');
+INSERT INTO permission_type VALUES(4, 'Access Confidential Notes');
+INSERT INTO permission_type VALUES(8, 'Merge Individuals');
+INSERT INTO permission_type VALUES(16, 'Edit Membership Status');
+
+INSERT INTO relationship_type VALUES(1, 'Parent');
+INSERT INTO relationship_type VALUES(2, 'Child');
+INSERT INTO relationship_type VALUES(3, 'Sibling');
+
+INSERT INTO marital_status_type VALUES(1, 'Single');
+INSERT INTO marital_status_type VALUES(2, 'Married');
+INSERT INTO marital_status_type VALUES(3, 'Separated');
+
+INSERT INTO marriage_status_type VALUES(1, 'Married');
+INSERT INTO marriage_status_type VALUES(2, 'Separated');
+INSERT INTO marriage_status_type VALUES(3, 'Divorced');
+INSERT INTO marriage_status_type VALUES(4, 'Widowed');
