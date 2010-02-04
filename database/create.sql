05d07508753b88dc1a8bfd6ea5bf62715c6e58c8
Mike Ho <mike.ho@alcf.net>
2010-02-04 09:53:48 -0800

    Updates for phone and address info

---
+++ database/create.sql
@@ -0,0 +1,324 @@
+/* SQLEditor (MySQL (2))*/
+
+CREATE TABLE login
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+role_type_id INTEGER UNSIGNED NOT NULL,
+username VARCHAR(40) UNIQUE,
+password_cache VARCHAR(200),
+password_last_set VARCHAR(200),
+domain_active_flag BOOLEAN,
+login_active_flag BOOLEAN,
+email VARCHAR(200) UNIQUE,
+first_name VARCHAR(100),
+middle_initial VARCHAR(1),
+last_name VARCHAR(100),
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE phone_type
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(40) NOT NULL UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE country
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(100),
+abbreviation VARCHAR(2) NOT NULL UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE address_type
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(40) NOT NULL UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE registry
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(80) NOT NULL UNIQUE,
+value TEXT,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE mug_shot
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+person_id INTEGER UNSIGNED NOT NULL,
+date_uploaded DATETIME NOT NULL,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE marriage_status_type
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(40) NOT NULL UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE marital_status_type
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(40) NOT NULL UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE household_participation
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+person_id INTEGER UNSIGNED NOT NULL,
+household_id INTEGER UNSIGNED NOT NULL,
+role VARCHAR(100),
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE relationship_type
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(40) NOT NULL UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE household
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(200),
+head_person_id INTEGER UNSIGNED NOT NULL UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE permission_type
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(40) NOT NULL UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE membership
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+person_id INTEGER UNSIGNED NOT NULL,
+date_start DATE NOT NULL,
+date_end DATE,
+termination_reason TEXT,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE membership_status_type
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(40) NOT NULL UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE marriage
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+person_id INTEGER UNSIGNED NOT NULL,
+married_to_person_id INTEGER UNSIGNED,
+marriage_status_type_id INTEGER UNSIGNED NOT NULL,
+date_start DATE,
+date_end DATE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE person
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+membership_status_type_id INTEGER UNSIGNED NOT NULL,
+marital_status_type_id INTEGER UNSIGNED,
+first_name VARCHAR(100),
+middle_nae VARCHAR(100),
+last_name VARCHAR(100),
+mailing_label VARCHAR(200),
+prior_last_names VARCHAR(255),
+nickname VARCHAR(100),
+title VARCHAR(40),
+suffix VARCHAR(40),
+male_flag BOOLEAN NOT NULL,
+date_of_birth DATE,
+dob_approximate_flag INTEGER,
+deceased_flag BOOLEAN NOT NULL,
+date_deceased DATE,
+current_mug_shot_id INTEGER UNSIGNED UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE relationship
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+person_id INTEGER UNSIGNED NOT NULL,
+related_to_person_id INTEGER UNSIGNED NOT NULL,
+relationship_type_id INTEGER UNSIGNED NOT NULL,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE ministry_login_assn
+(
+ministry_id INTEGER UNSIGNED NOT NULL,
+login_id INTEGER UNSIGNED NOT NULL,
+PRIMARY KEY (ministry_id,login_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE person_nameitem_assn
+(
+person_id INTEGER UNSIGNED NOT NULL,
+name_item_id INTEGER UNSIGNED NOT NULL,
+PRIMARY KEY (person_id,name_item_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE _version
+(
+version VARCHAR(20)
+) ENGINE=InnoDB;
+
+CREATE TABLE name_item
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(200) UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE role_type
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(40) NOT NULL UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE ministry
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+token VARCHAR(20) NOT NULL UNIQUE,
+name VARCHAR(100),
+parent_ministry_id INTEGER UNSIGNED,
+active_flag BOOLEAN NOT NULL,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE address
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+address_type_id INTEGER UNSIGNED NOT NULL,
+person_id INTEGER UNSIGNED,
+household_id INTEGER UNSIGNED,
+address_1 VARCHAR(200),
+address_2 VARCHAR(200),
+address_3 VARCHAR(200),
+city VARCHAR(100),
+state VARCHAR(100),
+zip_code VARCHAR(10),
+country VARCHAR(2),
+current_flag BOOLEAN,
+invalid_flag BOOLEAN,
+date_until_when DATE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE us_state
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+name VARCHAR(100),
+abbreviation VARCHAR(2) NOT NULL UNIQUE,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE phone
+(
+id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+phone_type_id INTEGER UNSIGNED NOT NULL,
+address_id INTEGER UNSIGNED,
+person_id INTEGER UNSIGNED,
+number VARCHAR(20),
+primary_flag BOOLEAN NOT NULL,
+PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE INDEX role_type_id_idx ON login(role_type_id);
+ALTER TABLE login ADD FOREIGN KEY role_type_id_idxfk (role_type_id) REFERENCES role_type (id);
+
+CREATE INDEX person_id_idx ON mug_shot(person_id);
+ALTER TABLE mug_shot ADD FOREIGN KEY person_id_idxfk (person_id) REFERENCES person (id);
+
+CREATE INDEX person_id_idx ON household_participation(person_id);
+ALTER TABLE household_participation ADD FOREIGN KEY person_id_idxfk_1 (person_id) REFERENCES person (id);
+
+CREATE INDEX household_id_idx ON household_participation(household_id);
+ALTER TABLE household_participation ADD FOREIGN KEY household_id_idxfk (household_id) REFERENCES household (id);
+
+CREATE UNIQUE INDEX household_participation_idx ON household_participation (person_id,household_id);
+
+ALTER TABLE household ADD FOREIGN KEY head_person_id_idxfk (head_person_id) REFERENCES person (id);
+
+CREATE INDEX person_id_idx ON membership(person_id);
+ALTER TABLE membership ADD FOREIGN KEY person_id_idxfk_2 (person_id) REFERENCES person (id);
+
+CREATE INDEX date_start_idx ON membership(date_start);
+CREATE INDEX membership_idx ON membership (person_id,date_end);
+
+CREATE INDEX person_id_idx ON marriage(person_id);
+ALTER TABLE marriage ADD FOREIGN KEY person_id_idxfk_3 (person_id) REFERENCES person (id);
+
+CREATE INDEX married_to_person_id_idx ON marriage(married_to_person_id);
+ALTER TABLE marriage ADD FOREIGN KEY married_to_person_id_idxfk (married_to_person_id) REFERENCES person (id);
+
+CREATE INDEX marriage_status_type_id_idx ON marriage(marriage_status_type_id);
+ALTER TABLE marriage ADD FOREIGN KEY marriage_status_type_id_idxfk (marriage_status_type_id) REFERENCES marriage_status_type (id);
+
+CREATE INDEX membership_status_type_id_idx ON person(membership_status_type_id);
+ALTER TABLE person ADD FOREIGN KEY membership_status_type_id_idxfk (membership_status_type_id) REFERENCES membership_status_type (id);
+
+CREATE INDEX marital_status_type_id_idx ON person(marital_status_type_id);
+ALTER TABLE person ADD FOREIGN KEY marital_status_type_id_idxfk (marital_status_type_id) REFERENCES marital_status_type (id);
+
+ALTER TABLE person ADD FOREIGN KEY current_mug_shot_id_idxfk (current_mug_shot_id) REFERENCES mug_shot (id);
+
+CREATE INDEX person_id_idx ON relationship(person_id);
+ALTER TABLE relationship ADD FOREIGN KEY person_id_idxfk_4 (person_id) REFERENCES person (id);
+
+CREATE INDEX related_to_person_id_idx ON relationship(related_to_person_id);
+ALTER TABLE relationship ADD FOREIGN KEY related_to_person_id_idxfk (related_to_person_id) REFERENCES person (id);
+
+CREATE INDEX relationship_type_id_idx ON relationship(relationship_type_id);
+ALTER TABLE relationship ADD FOREIGN KEY relationship_type_id_idxfk (relationship_type_id) REFERENCES relationship_type (id);
+
+CREATE UNIQUE INDEX relationship_idx ON relationship (person_id,related_to_person_id);
+
+ALTER TABLE ministry_login_assn ADD FOREIGN KEY ministry_id_idxfk (ministry_id) REFERENCES ministry (id);
+
+ALTER TABLE ministry_login_assn ADD FOREIGN KEY login_id_idxfk (login_id) REFERENCES login (id);
+
+ALTER TABLE person_nameitem_assn ADD FOREIGN KEY person_id_idxfk_5 (person_id) REFERENCES person (id);
+
+ALTER TABLE person_nameitem_assn ADD FOREIGN KEY name_item_id_idxfk (name_item_id) REFERENCES name_item (id);
+
+CREATE INDEX parent_ministry_id_idx ON ministry(parent_ministry_id);
+ALTER TABLE ministry ADD FOREIGN KEY parent_ministry_id_idxfk (parent_ministry_id) REFERENCES ministry (id);
+
+CREATE INDEX active_flag_idx ON ministry(active_flag);
+CREATE INDEX address_type_id_idx ON address(address_type_id);
+ALTER TABLE address ADD FOREIGN KEY address_type_id_idxfk (address_type_id) REFERENCES address_type (id);
+
+CREATE INDEX person_id_idx ON address(person_id);
+ALTER TABLE address ADD FOREIGN KEY person_id_idxfk_6 (person_id) REFERENCES person (id);
+
+CREATE INDEX household_id_idx ON address(household_id);
+ALTER TABLE address ADD FOREIGN KEY household_id_idxfk_1 (household_id) REFERENCES household (id);
+
+CREATE INDEX phone_type_id_idx ON phone(phone_type_id);
+ALTER TABLE phone ADD FOREIGN KEY phone_type_id_idxfk (phone_type_id) REFERENCES phone_type (id);
+
+CREATE INDEX address_id_idx ON phone(address_id);
+ALTER TABLE phone ADD FOREIGN KEY address_id_idxfk (address_id) REFERENCES address (id);
+
+CREATE INDEX person_id_idx ON phone(person_id);
+ALTER TABLE phone ADD FOREIGN KEY person_id_idxfk_7 (person_id) REFERENCES person (id);
+
+CREATE UNIQUE INDEX phone_idx ON phone (address_id,primary_flag);
+
+CREATE UNIQUE INDEX phone_idx_1 ON phone (person_id,primary_flag);
--- database/data.sql
+++ database/data.sql
@@ -1,6 +1,32 @@
-INSERT INTO role_type VALUES(1, 'Volunteer');
-INSERT INTO role_type VALUES(2, 'Staff Member');
-INSERT INTO role_type VALUES(3, 'ChMS Administrator');
+##############################
+# ChMS System Database Version
+##############################
+
+INSERT INTO _version VALUES('1.0');
+
+###############
+# Registry Data
+###############
+
+INSERT INTO registry VALUES(null, 'membership_termination_reason', 'Moved out of the area,Deceased,Switching to a different church');
+
+#################
+# Other Type Data
+#################
+
+INSERT INTO address_type VALUES(1, 'Home');
+INSERT INTO address_type VALUES(2, 'Work');
+INSERT INTO address_type VALUES(3, 'Other');
+INSERT INTO address_type VALUES(4, 'Temporary');
+
+INSERT INTO marital_status_type VALUES(1, 'Single');
+INSERT INTO marital_status_type VALUES(2, 'Married');
+INSERT INTO marital_status_type VALUES(3, 'Separated');
+
+INSERT INTO marriage_status_type VALUES(1, 'Married');
+INSERT INTO marriage_status_type VALUES(2, 'Separated');
+INSERT INTO marriage_status_type VALUES(3, 'Divorced');
+INSERT INTO marriage_status_type VALUES(4, 'Widowed');
 
 INSERT INTO membership_status_type VALUES(1, 'Non-Member');
 INSERT INTO membership_status_type VALUES(2, 'Member');
@@ -13,15 +39,16 @@ INSERT INTO permission_type VALUES(4, 'Access Confidential Notes');
 INSERT INTO permission_type VALUES(8, 'Merge Individuals');
 INSERT INTO permission_type VALUES(16, 'Edit Membership Status');
 
+INSERT INTO phone_type VALUES(1, 'Home');
+INSERT INTO phone_type VALUES(2, 'Work');
+INSERT INTO phone_type VALUES(3, 'Mobile');
+INSERT INTO phone_type VALUES(4, 'Fax');
+INSERT INTO phone_type VALUES(5, 'Other');
+
 INSERT INTO relationship_type VALUES(1, 'Parent');
 INSERT INTO relationship_type VALUES(2, 'Child');
 INSERT INTO relationship_type VALUES(3, 'Sibling');
 
-INSERT INTO marital_status_type VALUES(1, 'Single');
-INSERT INTO marital_status_type VALUES(2, 'Married');
-INSERT INTO marital_status_type VALUES(3, 'Separated');
-
-INSERT INTO marriage_status_type VALUES(1, 'Married');
-INSERT INTO marriage_status_type VALUES(2, 'Separated');
-INSERT INTO marriage_status_type VALUES(3, 'Divorced');
-INSERT INTO marriage_status_type VALUES(4, 'Widowed');
+INSERT INTO role_type VALUES(1, 'Volunteer');
+INSERT INTO role_type VALUES(2, 'Staff Member');
+INSERT INTO role_type VALUES(3, 'ChMS Administrator');
