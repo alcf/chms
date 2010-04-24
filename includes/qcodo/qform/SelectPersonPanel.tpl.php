1dd9120286bf40ee769305a977649e960f91e7ac
Mike Ho <mike.ho@alcf.net>
2010-04-23 17:11:12 -0700

    Small update to required flag

--- includes/qcodo/qform/SelectPersonPanel.tpl.php
+++ includes/qcodo/qform/SelectPersonPanel.tpl.php
@@ -4,6 +4,7 @@
 	else
 		$_CONTROL->txtFirstName->HtmlAfter = ' ' . $_CONTROL->txtLastName->Render(false);
 	$_CONTROL->txtFirstName->Name = $_CONTROL->Name;
+	$_CONTROL->txtFirstName->Required = $_CONTROL->Required;
 ?>
 <?php $_CONTROL->txtFirstName->RenderWithName(); ?>
 <?php $_CONTROL->dtgResults->RenderWithName(); ?>
