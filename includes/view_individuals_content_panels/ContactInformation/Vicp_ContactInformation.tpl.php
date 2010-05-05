f5ceee44da9e20c0601e9e6930d9d985ba152018
Mike Ho <mike.ho@alcf.net>
2010-05-04 18:13:22 -0700

    Added Add New

--- includes/view_individuals_content_panels/ContactInformation/Vicp_ContactInformation.tpl.php
+++ includes/view_individuals_content_panels/ContactInformation/Vicp_ContactInformation.tpl.php
@@ -1,5 +1,6 @@
 <?php if ($_FORM->objHousehold) { ?>
 	<h3>Home Addresses/Phone Information for <?php _p($_FORM->objHousehold->Name); ?></h3>
+	<p><a href="#contact/edit_home_address">Add New</a></p>
 	<?php $_CONTROL->dtgHomeAddresses->Render(); ?>
 <?php } ?>
 
