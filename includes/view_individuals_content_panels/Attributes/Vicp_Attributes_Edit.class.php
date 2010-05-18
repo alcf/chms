a04e95e799c0f2f930b8cd02cce3a9eb207cfb83
Mike Ho <mike.ho@alcf.net>
2010-05-18 16:37:48 -0700

    Implemented most data types for edit attribute

--- includes/view_individuals_content_panels/Attributes/Vicp_Attributes_Edit.class.php
+++ includes/view_individuals_content_panels/Attributes/Vicp_Attributes_Edit.class.php
@@ -7,8 +7,7 @@
 		public $calValue;
 		public $txtValue;
 		public $chkValue;
-		public $lstSingleValue;
-		public $lstMultipleValue;
+		public $lstValue;
 
 		public $txtAddItem;
 
@@ -28,31 +27,102 @@
 
 			switch ($this->objAttributeValue->Attribute->AttributeDataTypeId) {
 				case AttributeDataType::Checkbox:
+					$this->chkValue = new QRadioButtonList($this);
+					$this->chkValue->Name = $this->objAttributeValue->Attribute->Name;
+					$this->chkValue->Required = true;
+					$this->chkValue->AddItem('Yes', true, $this->objAttributeValue->BooleanValue === true);
+					$this->chkValue->AddItem('No', false, $this->objAttributeValue->BooleanValue === false);
 					break;
+
 				case AttributeDataType::Date:
+					$this->dtxValue = new QDateTimeTextBox($this);
+					$this->dtxValue->Name = $this->objAttributeValue->Attribute->Name;
+					$this->dtxValue->Required = true;
+					$this->dtxValue->Text = ($this->objAttributeValue->DateValue) ? $this->objAttributeValue->DateValue->__toString() : null;
+					$this->calValue = new QCalendar($this, $this->dtxValue);
+
+					$this->dtxValue->RemoveAllActions(QClickEvent::EventName);
 					break;
+
 				case AttributeDataType::Text:
+					$this->txtValue = new QTextBox($this);
+					$this->txtValue->Name = $this->objAttributeValue->Attribute->Name;
+					$this->txtValue->Required = true;
+					$this->txtValue->TextMode = QTextMode::MultiLine;
+					$this->txtValue->Text = trim($this->objAttributeValue->TextValue);
+					$this->txtValue->FontNames = QFontFamily::Arial;
+					$this->txtValue->FontSize = '11px';
+					$this->txtValue->Width = '400px';
+					$this->txtValue->Height = '200px';
 					break;
+
 				case AttributeDataType::ImmutableSingleDropdown:
-					$this->lstSingleValue = new QListBox($this);
-					$this->lstSingleValue->Name = $this->objAttributeValue->Attribute->Name;
-					$this->lstSingleValue->Required = true;
+					$this->lstValue = new QListBox($this);
+					$this->lstValue->Name = $this->objAttributeValue->Attribute->Name;
+					$this->lstValue->Required = true;
 					if (!$this->objAttributeValue->SingleAttributeOptionId)
-						$this->lstSingleValue->AddItem('- Select One -');
+						$this->lstValue->AddItem('- Select One -');
 					foreach ($this->objAttributeValue->Attribute->GetAttributeOptionArray(QQ::OrderBy(QQN::AttributeOption()->Name)) as $objOption)
-						$this->lstSingleValue->AddItem($objOption->Name, $objOption->Id, $objOption->Id == $this->objAttributeValue->SingleAttributeOptionId);
+						$this->lstValue->AddItem($objOption->Name, $objOption->Id, $objOption->Id == $this->objAttributeValue->SingleAttributeOptionId);
 					break;
+
 				case AttributeDataType::ImmutableMultipleDropdown:
+					$this->lstValue = new QListBox($this);
+					$this->lstValue->Name = $this->objAttributeValue->Attribute->Name;
+					$this->lstValue->Required = true;
+					$this->lstValue->SelectionMode = QSelectionMode::Multiple;
+					$this->lstValue->Width = '200px';
+					$this->lstValue->Height = '200px';
+
+					$intSelectedIdArray = array();
+					if ($this->objAttributeValue->Id) {
+						foreach ($this->objAttributeValue->GetAttributeOptionAsMultipleArray() as $objOption)
+							$intSelectedIdArray[$objOption->Id] = $objOption->Id;
+					}
+
+					foreach ($this->objAttributeValue->Attribute->GetAttributeOptionArray(QQ::OrderBy(QQN::AttributeOption()->Name)) as $objOption)
+						$this->lstValue->AddItem($objOption->Name, $objOption->Id, array_key_exists($objOption->Id, $intSelectedIdArray));
 					break;
+
 				case AttributeDataType::MutableSingleDropdown:
 					break;
+
 				case AttributeDataType::MutableMultipleDropdown:
+					$this->lstValue = new QListBox($this);
+					$this->lstValue->Name = $this->objAttributeValue->Attribute->Name;
+					$this->lstValue->Required = true;
+					$this->lstValue->SelectionMode = QSelectionMode::Multiple;
+					$this->lstValue->Width = '200px';
+					$this->lstValue->Height = '200px';
+
+					$intSelectedIdArray = array();
+					if ($this->objAttributeValue->Id) {
+						foreach ($this->objAttributeValue->GetAttributeOptionAsMultipleArray() as $objOption)
+							$intSelectedIdArray[$objOption->Id] = $objOption->Id;
+					}
+
+					foreach ($this->objAttributeValue->Attribute->GetAttributeOptionArray(QQ::OrderBy(QQN::AttributeOption()->Name)) as $objOption)
+						$this->lstValue->AddItem($objOption->Name, $objOption->Id, array_key_exists($objOption->Id, $intSelectedIdArray));
+
+					$this->txtAddItem = new QTextBox($this);
+					$this->txtAddItem->Name = 'Add an Option';
+					$this->txtAddItem->AddAction(new QEnterKeyEvent(), new QAjaxControlAction($this, 'txtAddItem_Enter'));
+					$this->txtAddItem->AddAction(new QEnterKeyEvent(), new QTerminateAction());
 					break;
+
 				default:
 					throw new Exception('Unhandled AttributeDataTypeId: ' . $this->objAttributeValue->Attribute->AttributeDataTypeId);
 			}
 		}
 	
+		public function txtAddItem_Enter($strFormId, $strControlId, $strParameter) {
+			if ($strName = trim($this->txtAddItem->Text)) {
+				$this->lstValue->AddItem($strName, -1 * count($this->lstValue->GetAllItems()), true);
+			}
+
+			$this->txtAddItem->Text = null;
+		}
+
 		public function btnCancel_Click($strFormId, $strControlId, $strParameter) {
 			return $this->ReturnTo('#attributes');
 		}
@@ -66,26 +136,51 @@
 
 			switch ($this->objAttributeValue->Attribute->AttributeDataTypeId) {
 				case AttributeDataType::Checkbox:
+					$this->objAttributeValue->BooleanValue = $this->chkValue->SelectedValue;
+					$this->objAttributeValue->Save();
 					break;
 
 				case AttributeDataType::Date:
+					$this->objAttributeValue->DateValue = $this->dtxValue->DateTime;
+					$this->objAttributeValue->Save();
 					break;
 
 				case AttributeDataType::Text:
+					$this->objAttributeValue->TextValue = trim($this->txtValue->Text);
+					$this->objAttributeValue->Save();
 					break;
 
 				case AttributeDataType::ImmutableSingleDropdown:
-					$this->objAttributeValue->SingleAttributeOptionId = $this->lstSingleValue->SelectedValue;
+					$this->objAttributeValue->SingleAttributeOptionId = $this->lstValue->SelectedValue;
 					$this->objAttributeValue->Save();
 					break;
 
 				case AttributeDataType::ImmutableMultipleDropdown:
+					$this->objAttributeValue->Save();
+					$this->objAttributeValue->UnassociateAllAttributeOptionsAsMultiple();
+					foreach ($this->lstValue->SelectedValues as $intOptionId) {
+						$this->objAttributeValue->AssociateAttributeOptionAsMultiple(AttributeOption::Load($intOptionId));
+					}
 					break;
 
 				case AttributeDataType::MutableSingleDropdown:
 					break;
 
 				case AttributeDataType::MutableMultipleDropdown:
+					$this->objAttributeValue->Save();
+					$this->objAttributeValue->UnassociateAllAttributeOptionsAsMultiple();
+					foreach ($this->lstValue->SelectedItems as $objListItem) {
+						$intOptionId = $objListItem->Value;
+						if ($intOptionId > 0)
+							$this->objAttributeValue->AssociateAttributeOptionAsMultiple(AttributeOption::Load($intOptionId));
+						else if ($objListItem->Selected) {
+							$objAttributeOption = new AttributeOption();
+							$objAttributeOption->AttributeId = $this->objAttributeValue->AttributeId;
+							$objAttributeOption->Name = $objListItem->Name;
+							$objAttributeOption->Save();
+							$this->objAttributeValue->AssociateAttributeOptionAsMultiple($objAttributeOption);
+						}
+					}
 					break;
 
 				default:
--- includes/view_individuals_content_panels/Attributes/Vicp_Attributes_Edit.tpl.php
+++ includes/view_individuals_content_panels/Attributes/Vicp_Attributes_Edit.tpl.php
@@ -1,11 +1,10 @@
 <h3><?php _p($_CONTROL->objAttributeValue->Id ? 'Create New' : 'Edit'); ?> Attribute Value: <?php _p($_CONTROL->objAttributeValue->Attribute->Name); ?></h3>
 
-<?php if ($_CONTROL->dtxValue) $_CONTROL->dtxValue->RenderWithName(); ?>
-<?php if ($_CONTROL->calValue) $_CONTROL->calValue->RenderWithName(); ?>
+<?php if ($_CONTROL->dtxValue) { $_CONTROL->dtxValue->HtmlAfter = '&nbsp;' . $_CONTROL->calValue->Render(false); $_CONTROL->dtxValue->RenderWithName(); } ?>
 <?php if ($_CONTROL->txtValue) $_CONTROL->txtValue->RenderWithName(); ?>
 <?php if ($_CONTROL->chkValue) $_CONTROL->chkValue->RenderWithName(); ?>
-<?php if ($_CONTROL->lstSingleValue) $_CONTROL->lstSingleValue->RenderWithName(); ?>
-<?php if ($_CONTROL->lstMultipleValue) $_CONTROL->lstMultipleValue->RenderWithName(); ?>
+<?php if ($_CONTROL->lstValue) $_CONTROL->lstValue->RenderWithName(); ?>
+<?php if ($_CONTROL->txtAddItem) $_CONTROL->txtAddItem->RenderWithName(); ?>
 
 <?php $_CONTROL->btnSave->Render(); ?> or <?php $_CONTROL->btnCancel->Render(); ?>
 
