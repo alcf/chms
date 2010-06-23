e392ce4762e8de7a605fbfb31f4e8a4c9de60c04
Mike Ho <mike.ho@alcf.net>
2010-06-23 13:01:26 -0700

    Small header/footer updates

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,8 +1,8 @@
 <div class="footer">
-Developed and Maintained by the <strong>ALCF Web Development Team</strong><br/>
+Developed and Maintained by the <a href="http://www.alcf.net/it/" class="footerLink" target="_blank">ALCF Web Development Team</a><br/>
 Copyright &copy; 2010
 &nbsp;&bull;&nbsp;
-<strong>Abundant Life Christian Fellowship</strong>, 2581 Leghorn Street, Mountain View, CA 94043
+<a href="http://www.alcf.net/" class="footerLink" target="_blank">Abundant Life Christian Fellowship</a>, 2581 Leghorn Street, Mountain View, CA 94043
 &nbsp;&bull;&nbsp;
 <a href="http://www.biblegateway.com/passage/?search=Romans%2012:1-8&version=31" target="_blank">Soli Deo Gloria</a>
 </div>
--- includes/header.inc.php
+++ includes/header.inc.php
@@ -19,7 +19,7 @@
 	<div class="status">
 		Welcome, <strong><?php _p(QApplication::$Login->Name); ?></strong>
 		&nbsp;|&nbsp;
-		<a href="#">My Profile</a>
+		<?php _p(QDateTime::Now()->ToString('DDDD, MMMM D YYYY')); ?>
 		&nbsp;|&nbsp;
 		<a href="/logout/">Logout</a>
 		<br/>
