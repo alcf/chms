845b256540d0e8b1287575e682dcafd20373583b
Mike Ho <mike.ho@alcf.net>
2010-06-30 11:22:07 -0700

    Implemented view page for comm lists

--- www/communications/index.php
+++ www/communications/index.php
@@ -2,26 +2,117 @@
 	require(dirname(__FILE__) . '/../../includes/prepend.inc.php');
 	QApplication::Authenticate();
 
-	class SearchEmailListsForm extends ChmsForm {
-		protected $strPageTitle = 'Email Lists';
+	class SearchCommunicationsForm extends ChmsForm {
+		protected $strPageTitle = 'View Email Lists';
 		protected $intNavSectionId = ChmsForm::NavSectionCommunications;
 
-		protected $lblMessage;
-		protected $btnButton;
+		protected $lblMinistry;
+		protected $pnlMinistries;
+		protected $pxyMinistry;
+		protected $intMinistryId;
+
+		protected $dtgCommLists;
+		protected $lblStartText;
 
 		protected function Form_Create() {
-			$this->lblMessage = new QLabel($this);
-			$this->lblMessage->Text = 'Click the button to change my message.';
+			$this->pnlMinistries = new QPanel($this);
+			$this->pnlMinistries->TagName = 'ul';
+			$this->pnlMinistries->CssClass = 'subnavSide subnavSideSmall';
+			$this->pnlMinistries->AutoRenderChildren = true;
 
-			$this->btnButton = new QButton($this);
-			$this->btnButton->Text = 'Click Me';
-			$this->btnButton->AddAction(new QClickEvent(), new QAjaxAction('btnButton_Click'));
+			$this->pxyMinistry = new QControlProxy($this);
+			$this->pxyMinistry->AddAction(new QClickEvent(), new QAjaxAction('pxyMinistry_Click'));
+			$this->pxyMinistry->AddAction(new QClickEvent(), new QTerminateAction());
+
+			$blnFirst = true;
+			foreach (Ministry::LoadArrayByActiveFlag(true, QQ::OrderBy(QQN::Ministry()->Name)) as $objMinistry) {
+				$pnlMinistry = new QPanel($this->pnlMinistries, 'pnlMinistry' . $objMinistry->Id);
+				$pnlMinistry->TagName = 'li';
+				$pnlMinistry->ActionParameter = $objMinistry->Id;
+				if ($blnFirst) {
+					$blnFirst = false;
+					$pnlMinistry->CssClass = 'first';
+				}
+				$this->pnlMinistry_Refresh($objMinistry);
+			}
+			// Last
+			$pnlMinistry->CssClass = 'last';
+			
+			$this->lblMinistry = new QLabel($this);
+			$this->lblMinistry->TagName = 'h3';
+			$this->lblMinistry_Refresh();
+
+			$this->dtgCommLists = new CommunicationListDataGrid($this);
+			$this->dtgCommLists->MetaAddColumn('Name', 'Name=List Name', 'Html=<?= $_FORM->RenderName($_ITEM); ?>', 'HtmlEntities=false', 'Width=260px');
+			$this->dtgCommLists->MetaAddTypeColumn('EmailBroadcastTypeId', 'EmailBroadcastType', 'Name=List Type', 'Width=120px');
+			$this->dtgCommLists->MetaAddColumn('Token', 'Name=Email', 'Html=<?= $_FORM->RenderEmail($_ITEM); ?>', 'HtmlEntities=false', 'Width=350px');
+			$this->dtgCommLists->SetDataBinder('dtgCommLists_Bind');
+			$this->dtgCommLists->NoDataHtml = '<p>There are no Email Lists for this ministry.</p>';
+			
+			$this->lblStartText = new QLabel($this);
+			$this->lblStartText->Text = '<h3>Email Lists in Ministries</h3><p>Please select an ministry from the list on the right.</p>';
+			$this->lblStartText->HtmlEntities = false;
 		}
 
-		protected function btnButton_Click($strFormId, $strControlId, $strParameter) {
-			$this->lblMessage->Text = 'Hello, World!';
+		public function dtgCommLists_Bind() {
+			if ($this->intMinistryId) {
+				$this->dtgCommLists->MetaDataBinder(QQ::Equal(QQN::CommunicationList()->MinistryId, $this->intMinistryId));
+				$this->dtgCommLists->Visible = true;
+			} else {
+				$this->dtgCommLists->DataSource = null;
+				$this->dtgCommLists->Visible = false;
+			}
+
+			$this->lblStartText->Visible = !$this->dtgCommLists->Visible;
+		}
+
+		public function RenderName(CommunicationList $objList) {
+			return sprintf('<a href="/communications/list.php/%s">%s</a>', $objList->Id, QApplication::HtmlEntities($objList->Name));
+		}
+		
+		public function RenderEmail(CommunicationList $objList) {
+			return sprintf('<a href="mailto:%s@groups.alcf.net">%s@groups.alcf.net</a>', $objList->Token, $objList->Token);
+		}
+		
+		protected function pnlMinistry_Refresh($mixMinistry) {
+			if ($mixMinistry instanceof Ministry) {
+				$objMinistry = $mixMinistry;
+				$intMinistryId = $objMinistry->Id;
+			} else {
+				$intMinistryId = $mixMinistry;
+				$objMinistry = Ministry::Load($intMinistryId);
+			}
+
+			$pnlMinistry = $this->GetControl('pnlMinistry' . $intMinistryId);
+			if ($pnlMinistry) {
+				$pnlMinistry->Text = sprintf('<a href="" %s %s>%s</a>',
+					$this->pxyMinistry->RenderAsEvents($objMinistry->Id, false),
+					($intMinistryId == $this->intMinistryId) ? 'class="selected"' : null,
+					$objMinistry->Name);
+			}
+		}
+		
+		protected function lblMinistry_Refresh() {
+			$objMinistry = Ministry::Load($this->intMinistryId);
+			if ($objMinistry) {
+				$this->lblMinistry->Visible = true;
+				$this->lblMinistry->Text = 'Email Lists in ' . $objMinistry->Name;
+			} else {
+				$this->lblMinistry->Visible = false;
+			}
+		}
+
+		protected function pxyMinistry_Click($strFormId, $strControlId, $strParameter) {
+			if ($strParameter != $this->intMinistryId) {
+				$intOldMinistryId = $this->intMinistryId;
+				$this->intMinistryId = $strParameter;
+				$this->pnlMinistry_Refresh($intOldMinistryId);
+				$this->pnlMinistry_Refresh($this->intMinistryId);
+				$this->lblMinistry_Refresh();
+				$this->dtgCommLists->Refresh();
+			}
 		}
 	}
 
-	SearchEmailListsForm::Run('SearchEmailListsForm');
+	SearchCommunicationsForm::Run('SearchCommunicationsForm');
 ?>
--- www/communications/index.tpl.php
+++ www/communications/index.tpl.php
@@ -1,6 +1,13 @@
 <?php require(__INCLUDES__ . '/header.inc.php'); ?>
 
-	<p><?php $this->lblMessage->Render(); ?></p>
-	<p><?php $this->btnButton->Render(); ?></p>
-
+	<h1>Email Lists</h1>
+	<?php $this->pnlMinistries->Render(); ?>
+	<div class="subnavContent">
+		<?php $this->lblMinistry->Render(); ?>
+		<div class="section">
+			<?php $this->dtgCommLists->Render(); ?>
+			<?php $this->lblStartText->Render(); ?>
+		</div>
+	</div>
+	<br clear="all"/>
 <?php require(__INCLUDES__ . '/footer.inc.php'); ?>
