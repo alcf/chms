20fae96c4945caf776f1565c1c1ce881a7f814ae
Mike Ho <mike.ho@alcf.net>
2010-06-30 16:23:27 -0700

    Start of admin screen index

--- www/admin/index.tpl.php
+++ www/admin/index.tpl.php
@@ -1,6 +1,9 @@
 <?php require(__INCLUDES__ . '/header.inc.php'); ?>
-
-	<p><?php $this->lblMessage->Render(); ?></p>
-	<p><?php $this->btnButton->Render(); ?></p>
+	<h1>ChMS Administration Tools</h1>
+	<div class="section">
+		<button class="primary" style="margin-left:  0px;" onclick="document.location=     '/admin/users.php'; return false;">ChMS Users</button>
+		<button class="primary" style="margin-left: 25px;" onclick="document.location='/admin/ministries.php'; return false;">Ministries</button>
+		<button class="primary" style="margin-left: 25px;" onclick="document.location='/admin/attributes.php'; return false;">Individuals' Attributes</button>
+	</div>
 
 <?php require(__INCLUDES__ . '/footer.inc.php'); ?>
