e379b3912093bee52c0a0c7a3990b0ec411c2682
Mike Ho <mike.ho@alcf.net>
2010-08-17 00:26:33 -0700

    Implemented stack count list control

--- includes/content_panels/stewardship/CpStewardship_EditBatch.class.php
+++ includes/content_panels/stewardship/CpStewardship_EditBatch.class.php
@@ -3,10 +3,42 @@
 		public $mctStewardshipBatch;
 
 		public $txtDescription;
+		public $lstStackCount;
+		
+		public $txtReportedTotals;
 
 		protected function SetupPanel() {
 			$this->mctStewardshipBatch = new StewardshipBatchMetaControl($this, $this->objBatch);
 			$this->txtDescription = $this->mctStewardshipBatch->txtDescription_Create();
+
+			$this->lstStackCount = new QListBox($this);
+			$this->lstStackCount->Name = 'Number of Stacks';
+
+			$objStackArray = array();
+			foreach ($this->objBatch->GetStewardshipStackArray() as $objStack) {
+				$objStackArray[$objStack->StackNumber] = $objStack;
+			}
+
+			$this->txtReportedTotals = array();
+			for ($i = 1; $i <= 10; $i++) {
+				$txtReportedTotal = new QFloatTextBox($this, 'txtReportedTotal' . $i);
+				$txtReportedTotal->Name = 'Reported Total for Stack #' . $i;
+				$txtReportedTotal->Visible = false;
+				if (array_key_exists($i, $objStackArray)) $txtReportedTotal->Text = $objStackArray[$i]->ReportedTotalAmount;
+				$this->txtReportedTotals[$i] = $txtReportedTotal;
+			}
+			
+			for ($i = max(1, count($objStackArray)); $i <= 10; $i++) {
+				$this->lstStackCount->AddItem($i, $i, $i==max(1, count($objStackArray)));
+			}
+			$this->lstStackCount->AddAction(new QChangeEvent(), new QAjaxControlAction($this, 'lstStackCount_Change'));
+			$this->lstStackCount_Change();
+		}
+		
+		public function lstStackCount_Change() {
+			foreach ($this->txtReportedTotals as $i => $txtReportedTotal) {
+				$txtReportedTotal->Visible = ($this->lstStackCount->SelectedValue >= $i);
+			}
 		}
 	}
 ?>
--- includes/content_panels/stewardship/CpStewardship_EditBatch.tpl.php
+++ includes/content_panels/stewardship/CpStewardship_EditBatch.tpl.php
@@ -1,3 +1,15 @@
 <div class="section">
 	<?php $_CONTROL->txtDescription->RenderWithName(); ?>
+	<?php $_CONTROL->lstStackCount->RenderWithName(); ?>
+</div>
+
+<h3>Reported Stack Totals</h3>
+<div class="section">
+	<?php foreach ($_CONTROL->txtReportedTotals as $txtReportedTotal) $txtReportedTotal->RenderWithName(); ?>
+</div>
+
+<div class="buttonBar">
+	<?php $_CONTROL->btnSave->Render(); ?>
+	 &nbsp;or&nbsp; 
+	<?php $_CONTROL->btnCancel->Render(); ?>
 </div>
