26fb585d3418c5bdb157252ffc602e5b0322b701
Mike Ho <mike.ho@alcf.net>
2011-01-18 22:52:43 -0800

    Updated copyright in footer

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,6 +1,6 @@
 <div class="footer">
 Developed and Maintained by the <a href="http://www.alcf.net/it/" class="footerLink" target="_blank">ALCF Web Development Team</a><br/>
-Copyright &copy; 2010
+Copyright &copy; 2010 - <?php _p(date('Y')); ?>
 &nbsp;&bull;&nbsp;
 <a href="http://www.alcf.net/" class="footerLink" target="_blank">Abundant Life Christian Fellowship</a>, 2581 Leghorn Street, Mountain View, CA 94043
 &nbsp;&bull;&nbsp;
