09cc621a8989cabd7322b653c26c40c8d7cc525b
Mike Ho <mike.ho@alcf.net>
2011-07-05 17:17:32 -0700

    Restructured stewardship pages and added menu

---
+++ www/stewardship/batches.tpl.php
@@ -0,0 +1,17 @@
+<?php require(__INCLUDES__ . '/header.inc.php'); ?>
+	<h1>Stewardship - View All Batches
+		<button class="primary" onclick="document.location='/stewardship/new.php'; return false;">Create New Batch</button>
+	</h1>	
+	
+	<div class="section">
+		<div class="filterBy">Filter by Status<br/><?php $this->lstStatus->Render(); ?></div>
+		<div class="filterBy">Description<br/><?php $this->txtDescription->Render('Width=300px'); ?></div>
+		<div class="filterBy">Created By<br/><?php $this->lstCreatedBy->Render(); ?></div>
+		<div class="cleaner">&nbsp;</div>
+	</div>
+
+	<div class="section">
+		<?php $this->dtgBatches->Render(); ?>
+	</div>
+
+<?php require(__INCLUDES__ . '/footer.inc.php'); ?>
--- www/stewardship/index.tpl.php
+++ www/stewardship/index.tpl.php
@@ -1,37 +1,31 @@
 <?php require(__INCLUDES__ . '/header.inc.php'); ?>
-	<h1>Stewardship - View All Batches
-		<button class="primary" onclick="document.location='/stewardship/new.php'; return false;">Create New Batch</button>
-	</h1>	
+	<h1>NOAH Stewardship and Financial Tools</h1>
+	<div class="section">
+		<h3>Stewardship/Contributions</h3>
+		<button class="primary" style="margin-left:  0px;" onclick="document.location='/stewardship/batches.php'; return false;">View All Batches</button>
+		<button class="primary" style="margin-left: 25px;" onclick="document.location='/stewardship/search/'; return false;">Search for a Contribution</button>
+	</div>
 	
 	<div class="section">
-		<div class="filterBy">Filter by Status<br/><?php $this->lstStatus->Render(); ?></div>
-		<div class="filterBy">Description<br/><?php $this->txtDescription->Render('Width=300px'); ?></div>
-		<div class="filterBy">Created By<br/><?php $this->lstCreatedBy->Render(); ?></div>
-		<div class="cleaner">&nbsp;</div>
+		<h3>Reports</h3>
+		<p>
+			Report: Invalid Addresses for <a href="/stewardship/reports/invalid_addresses.php/<?php _p(QDateTime::Now()->Year-1); ?>" class="cancel"><?php _p(QDateTime::Now()->Year - 1); ?></a>
+			or <a href="/stewardship/reports/invalid_addresses.php/<?php _p(QDateTime::Now()->Year); ?>" class="cancel"><?php _p(QDateTime::Now()->Year); ?></a>
+			&nbsp;|&nbsp;
+			Report: Duplicate Names for <a href="/stewardship/reports/duplicate_names.php/<?php _p(QDateTime::Now()->Year-1); ?>" class="cancel"><?php _p(QDateTime::Now()->Year - 1); ?></a>
+			or <a href="/stewardship/reports/duplicate_names.php/<?php _p(QDateTime::Now()->Year); ?>" class="cancel"><?php _p(QDateTime::Now()->Year); ?></a>
+		</p>
+		<p>
+			<a href="/stewardship/reports/monthly_giving.php" class="cancel">Monthly Giving Report</a>
+			&nbsp;|&nbsp;
+			<a href="/stewardship/pledges/" class="cancel">View Outstanding Pledges</a>
+		</p>
 	</div>
 
 	<div class="section">
-		<?php $this->dtgBatches->Render(); ?>
+		<h3>Tools</h3>
+		<button class="primary" style="margin-left: 0px;" onclick="document.location= '/stewardship/receipts/'; return false;">Generate Bulk Receipts</button>
+		<button class="primary" style="margin-left: 25px;" onclick="document.location='/stewardship/funds/'; return false;">View/Edit Stewardship Funding Accounts</button>
 	</div>
-
-	<div class="buttonBar" style="color: #666; text-align: center;">
-		<a href="/stewardship/search/" class="cancel">Search for a Transaction</a>
-		&nbsp;|&nbsp;
-		Report: Invalid Addresses for <a href="/stewardship/reports/invalid_addresses.php/<?php _p(QDateTime::Now()->Year-1); ?>" class="cancel"><?php _p(QDateTime::Now()->Year - 1); ?></a>
-		or <a href="/stewardship/reports/invalid_addresses.php/<?php _p(QDateTime::Now()->Year); ?>" class="cancel"><?php _p(QDateTime::Now()->Year); ?></a>
-		&nbsp;|&nbsp;
-		Report: Duplicate Names for <a href="/stewardship/reports/duplicate_names.php/<?php _p(QDateTime::Now()->Year-1); ?>" class="cancel"><?php _p(QDateTime::Now()->Year - 1); ?></a>
-		or <a href="/stewardship/reports/duplicate_names.php/<?php _p(QDateTime::Now()->Year); ?>" class="cancel"><?php _p(QDateTime::Now()->Year); ?></a>
-
-		<br/><br/>
-		<a href="/stewardship/reports/monthly_giving.php" class="cancel">Monthly Giving Report</a>
-		&nbsp;|&nbsp;
-		<a href="/stewardship/pledges/" class="cancel">View Outstanding Pledges</a>
-		&nbsp;|&nbsp;
-		<a href="/stewardship/receipts/" class="cancel">View/Generate Bulk Receipts</a>
-		&nbsp;|&nbsp;
-		<a href="/stewardship/funds/" class="cancel">View/Edit Funds</a>
-	</div>
-	<br/><br/>
 
 <?php require(__INCLUDES__ . '/footer.inc.php'); ?>
