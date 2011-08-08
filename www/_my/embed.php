ea122d66d8625c8f76c520471fda6bd64fa1e72f
Mike Ho <mike.ho@alcf.net>
2011-08-08 13:14:21 -0700

    Changed login/logout for embed script to be proper verbs

--- www/_my/embed.php
+++ www/_my/embed.php
@@ -7,7 +7,7 @@
 		var objUtility = document.getElementById("utility");
 		var strHtml = objUtility.innerHTML;
 
-		strHtml = strHtml.replace('<a href="/login.asp">Login</a>', '<a href="https://my.alcf.net/logout/">Logout</a>');
+		strHtml = strHtml.replace('<a href="/login.asp">Login</a>', '<a href="https://my.alcf.net/logout/">Log Out</a>');
 		strHtml = strHtml.replace('<a href="/register.asp">Register</a>', '<a href="https://my.alcf.net/main/">My Profile</a>');
 
 		objUtility.innerHTML = strHtml;
@@ -17,7 +17,7 @@
 		var objUtility = document.getElementById("utility");
 		var strHtml = objUtility.innerHTML;
 
-		strHtml = strHtml.replace('<a href="/login.asp">Login</a>', '<a href="https://my.alcf.net/">Login</a>');
+		strHtml = strHtml.replace('<a href="/login.asp">Login</a>', '<a href="https://my.alcf.net/">Log In</a>');
 		strHtml = strHtml.replace('<a href="/register.asp">Register</a>', '<a href="https://my.alcf.net/register/">Register</a>');
 
 		objUtility.innerHTML = strHtml;
