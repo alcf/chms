2bf6232cf522e03302bd72f17a1dec13415331db
Mike Ho <mike.ho@alcf.net>
2012-03-13 14:33:18 -0700

    Implemented unasosciating of ppi records from person

--- includes/view_individuals_content_panels/SafariKids/Vicp_SafariKids.class.php
+++ includes/view_individuals_content_panels/SafariKids/Vicp_SafariKids.class.php
@@ -3,6 +3,8 @@
 		public $dtgAttendantHistory;
 		public $dtgChildHistory;
 		public $dtgParentHistory;
+		public $dtgParentPagerIndividuals;
+		public $pxyUnassociate;
 		
 		protected function SetupPanel() {
 			if ($this->objPerson->CountParentPagerIndividuals()) {
@@ -37,9 +39,51 @@
 					$this->dtgChildHistory->SortDirection = 1;
 					$this->dtgChildHistory->FontSize = '11px';
 				}
+	
+				$this->dtgParentPagerIndividuals = new ParentPagerIndividualDataGrid($this);
+				$this->dtgParentPagerIndividuals->MetaAddColumn('ServerIdentifier', 'Name=Parent Pager ID', 'Html=<?= $_CONTROL->ParentControl->RenderIdentifier($_ITEM); ?>', 'Width=120px', 'HtmlEntities=false');
+				$this->dtgParentPagerIndividuals->MetaAddColumn('FirstName', 'Width=125px', 'FontSize=10px');
+				$this->dtgParentPagerIndividuals->MetaAddColumn('MiddleName', 'Width=125px', 'FontSize=10px');
+				$this->dtgParentPagerIndividuals->MetaAddColumn('LastName', 'Width=125px', 'FontSize=10px');
+				$this->dtgParentPagerIndividuals->AddColumn(new QDataGridColumn('Address(es)', '<?= $_CONTROL->ParentControl->RenderAddresses($_ITEM); ?>', 'Width=200px', 'HtmlEntities=false'));
+				$this->dtgParentPagerIndividuals->SetDataBinder('ParentPagerIndividuals_Bind', $this);
+				$this->dtgParentPagerIndividuals->SortColumnIndex = 0;
+				$this->dtgParentPagerIndividuals->FontSize = '11px';
 			}
+			
+			$this->pxyUnassociate = new QControlProxy($this);
+			$this->pxyUnassociate->AddAction(new QClickEvent(), new QConfirmAction('Are you SURE you want to UNASSOCIATE this ParentPagerIndividual record from this NOAH Person?'));
+			$this->pxyUnassociate->AddAction(new QClickEvent(), new QAjaxControlAction($this, 'pxyUnassociate_Click'));
+			$this->pxyUnassociate->AddAction(new QClickEvent(), new QTerminateAction());
 		}
+
+		public function pxyUnassociate_Click($strFormId, $strControlId, $strParameter) {
+			$objIndividual = ParentPagerIndividual::Load($strParameter);
+			$objIndividual->Person = null;
+			$objIndividual->Save();
+			$this->Refresh();
+		}
+
+		public function RenderIdentifier(ParentPagerIndividual $objIndividual) {
+			return sprintf('<a href="#" %s>%s</a>', $this->pxyUnassociate->RenderAsEvents($objIndividual->Id, false), $objIndividual->Id);
+		}
+
+		public function RenderAddresses(ParentPagerIndividual $objIndividual) {
+			$objAddressArray = $objIndividual->GetParentPagerAddressArray();
+			if ($objIndividual->ParentPagerHousehold)
+				$objAddressArray = array_merge($objAddressArray, $objIndividual->ParentPagerHousehold->GetParentPagerAddressArray());
 		
+			$strToReturnArray = array();
+			foreach ($objAddressArray as $objAddress) {
+				$strToReturnArray[] = QApplication::HtmlEntities($objAddress->Address1) . ', ' . QApplication::HtmlEntities($objAddress->City);
+			}
+			return implode('<br/>', $strToReturnArray);
+		}
+
+		public function ParentPagerIndividuals_Bind() {
+			$this->dtgParentPagerIndividuals->MetaDataBinder(QQ::Equal(QQN::ParentPagerIndividual()->PersonId, $this->objPerson->Id));
+		}
+
 		public function dtgAttendantHistory_Bind() {
 			$this->dtgAttendantHistory->MetaDataBinder(QQ::Equal(QQN::ParentPagerAttendantHistory()->ParentPagerIndividual->PersonId, $this->objPerson->Id));
 		}
--- includes/view_individuals_content_panels/SafariKids/Vicp_SafariKids.tpl.php
+++ includes/view_individuals_content_panels/SafariKids/Vicp_SafariKids.tpl.php
@@ -1,6 +1,6 @@
 <h3>Safari Kids / ParentPager Information</h3>
 
-<?php if (!$_CONTROL->dtgAttendantHistory && !$_CONTROL->dtgChildHistory) { ?>
+<?php if (!$_CONTROL->dtgParentPagerIndividuals) { ?>
 	<div class="section">
 		<p><em>Not currently associated with a ParentPager record.</em></p>
 		<p><a href="#sk/select">Associate a ParentPager Record</a> to <?php _p($this->objPerson->Name); ?>'s NOAH Account.</p>
@@ -19,6 +19,11 @@
 			<?php $_CONTROL->dtgChildHistory->Render(); ?>
 		</div>
 	<?php } ?>
+
+	<div class="section">
+		<h3>Associated ParentPagerIndividual Records</h3>
+		<?php $_CONTROL->dtgParentPagerIndividuals->Render(); ?>
+	</div>
 
 	<div class="buttonBar">
 		<a href="#sk/select" class="cancel">Associate <em>another</em> ParentPager Record to <?php _p($this->objPerson->Name); ?>'s NOAH Account.</a>
