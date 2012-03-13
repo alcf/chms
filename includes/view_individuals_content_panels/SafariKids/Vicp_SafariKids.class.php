b896ca8e9bbc26c5de05b9b47d7c32fd12565ad0
Mike Ho <mike.ho@alcf.net>
2012-03-13 14:21:51 -0700

    Implemented view of logs and association management for parentpager

--- includes/view_individuals_content_panels/SafariKids/Vicp_SafariKids.class.php
+++ includes/view_individuals_content_panels/SafariKids/Vicp_SafariKids.class.php
@@ -1,12 +1,55 @@
 <?php
 	class Vicp_SafariKids extends Vicp_Base {
-		public $dtgAttendentHistory;
+		public $dtgAttendantHistory;
 		public $dtgChildHistory;
-
+		public $dtgParentHistory;
+		
 		protected function SetupPanel() {
 			if ($this->objPerson->CountParentPagerIndividuals()) {
+				if (ParentPagerAttendantHistory::QueryCount(QQ::Equal(QQN::ParentPagerAttendantHistory()->ParentPagerIndividual->PersonId, $this->objPerson->Id))) {
+					$this->dtgAttendantHistory = new ParentPagerAttendantHistoryDataGrid($this);
+					$this->dtgAttendantHistory->MetaAddColumn('DateIn', 'Width=125px', 'FontSize=10px');
+					$this->dtgAttendantHistory->MetaAddColumn('DateOut', 'Width=125px', 'FontSize=10px');
+					$this->dtgAttendantHistory->MetaAddColumn(QQN::ParentPagerAttendantHistory()->ParentPagerStation->Name, 'Name=Station', 'Width=160px');
+					$this->dtgAttendantHistory->MetaAddColumn(QQN::ParentPagerAttendantHistory()->ParentPagerPeriod->Name, 'Name=Period', 'Width=150px');
+					$this->dtgAttendantHistory->MetaAddColumn(QQN::ParentPagerAttendantHistory()->ParentPagerProgram->Name, 'Name=Program', 'Width=150px');
+					$this->dtgAttendantHistory->SetDataBinder('dtgAttendantHistory_Bind', $this);
+					$this->dtgAttendantHistory->Paginator = new QPaginator($this->dtgAttendantHistory);
+					$this->dtgAttendantHistory->SortColumnIndex = 0;
+					$this->dtgAttendantHistory->SortDirection = 1;
+					$this->dtgAttendantHistory->FontSize = '11px';
+				}
 				
+				if (ParentPagerChildHistory::QueryCount(QQ::Equal(QQN::ParentPagerChildHistory()->ParentPagerIndividual->PersonId, $this->objPerson->Id)) ||
+					ParentPagerChildHistory::QueryCount(QQ::Equal(QQN::ParentPagerChildHistory()->PickupByParentPagerIndividual->PersonId, $this->objPerson->Id)) ||
+					ParentPagerChildHistory::QueryCount(QQ::Equal(QQN::ParentPagerChildHistory()->DropoffByParentPagerIndividual->PersonId, $this->objPerson->Id))) {
+					$this->dtgChildHistory = new ParentPagerChildHistoryDataGrid($this);
+					$this->dtgChildHistory->MetaAddColumn('DateIn', 'Width=125px', 'FontSize=10px');
+					$this->dtgChildHistory->MetaAddColumn('DateOut', 'Width=125px', 'FontSize=10px');
+					$this->dtgChildHistory->MetaAddColumn(QQN::ParentPagerChildHistory()->ParentPagerIndividual->FirstName, 'Name=Child', 'Width=80px');
+					$this->dtgChildHistory->MetaAddColumn(QQN::ParentPagerChildHistory()->PickupByParentPagerIndividual->FirstName, 'Name=Pick Up', 'Width=70px');
+					$this->dtgChildHistory->MetaAddColumn(QQN::ParentPagerChildHistory()->DropoffByParentPagerIndividual->FirstName, 'Name=Drop Off', 'Width=70px');
+					$this->dtgChildHistory->MetaAddColumn(QQN::ParentPagerChildHistory()->ParentPagerStation->Name, 'Name=Station', 'Width=150px');
+					$this->dtgChildHistory->MetaAddColumn(QQN::ParentPagerChildHistory()->ParentPagerPeriod->Name, 'Name=Period', 'Width=70px');
+					$this->dtgChildHistory->SetDataBinder('dtgChildHistory_Bind', $this);
+					$this->dtgChildHistory->Paginator = new QPaginator($this->dtgChildHistory);
+					$this->dtgChildHistory->SortColumnIndex = 0;
+					$this->dtgChildHistory->SortDirection = 1;
+					$this->dtgChildHistory->FontSize = '11px';
+				}
 			}
+		}
+		
+		public function dtgAttendantHistory_Bind() {
+			$this->dtgAttendantHistory->MetaDataBinder(QQ::Equal(QQN::ParentPagerAttendantHistory()->ParentPagerIndividual->PersonId, $this->objPerson->Id));
+		}
+
+		public function dtgChildHistory_Bind() {
+			$this->dtgChildHistory->MetaDataBinder(QQ::OrCondition(
+				QQ::Equal(QQN::ParentPagerChildHistory()->ParentPagerIndividual->PersonId, $this->objPerson->Id),
+				QQ::Equal(QQN::ParentPagerChildHistory()->PickupByParentPagerIndividual->PersonId, $this->objPerson->Id),
+				QQ::Equal(QQN::ParentPagerChildHistory()->DropoffByParentPagerIndividual->PersonId, $this->objPerson->Id)
+			));
 		}
 	}
 ?>
--- includes/view_individuals_content_panels/SafariKids/Vicp_SafariKids.tpl.php
+++ includes/view_individuals_content_panels/SafariKids/Vicp_SafariKids.tpl.php
@@ -1,8 +1,27 @@
 <h3>Safari Kids / ParentPager Information</h3>
 
-<?php if (!$_CONTROL->dtgAttendentHistory && !$_CONTROL->dtgChildHistory) { ?>
+<?php if (!$_CONTROL->dtgAttendantHistory && !$_CONTROL->dtgChildHistory) { ?>
 	<div class="section">
 		<p><em>Not currently associated with a ParentPager record.</em></p>
 		<p><a href="#sk/select">Associate a ParentPager Record</a> to <?php _p($this->objPerson->Name); ?>'s NOAH Account.</p>
 	</div>
+<?php } else { ?>
+
+	<?php if ($_CONTROL->dtgAttendantHistory) { ?>
+		<div class="section">
+			<h3>History as an Attendant</h3>
+			<?php $_CONTROL->dtgAttendantHistory->Render(); ?>
+		</div>
+	<?php } ?>
+	<?php if ($_CONTROL->dtgChildHistory) { ?>
+		<div class="section">
+			<h3>History as a Parent or Child</h3>
+			<?php $_CONTROL->dtgChildHistory->Render(); ?>
+		</div>
+	<?php } ?>
+
+	<div class="buttonBar">
+		<a href="#sk/select" class="cancel">Associate <em>another</em> ParentPager Record to <?php _p($this->objPerson->Name); ?>'s NOAH Account.</a>
+	</div>
+
 <?php } ?>
