83331e99345e67700ac2b046e36cb877500d7069
Gareth Seeto <gsee@localhost.localdomain>
2013-10-08 16:03:11 -0700

    Updating Growth Group page look and feel

--- www/_growthgroups/dtrGrowthGroups.tpl.php
+++ www/_growthgroups/dtrGrowthGroups.tpl.php
@@ -1,10 +1,14 @@
 <div class="gglocation">
+<div class='name'>
 	<div class="marker"><a href="javascript:markerclick(<?php _p($this->intMarkerArray[$_ITEM->GroupId] - 1); ?>)" ><img src="/images/mapfiles/marker<?php _p($this->intMarkerArray[$_ITEM->GroupId]); ?>.png"/></a></div>
-	<div class="content">
-		<strong><?php _p($_ITEM->Group->Name); ?></strong><br/>
-		<?php _p($_ITEM->Meetings); ?><br/>
-		<?php _p($_ITEM->Times); ?><br/>
-		<?php _p($_ITEM->StructuresHtml, false); ?><br/>
-		<?php _p($_ITEM->Description); ?>
-	</div>
+	
+<strong><?php _p($_ITEM->Group->Name); ?></strong></div>
+<div class='description'>			<?php _p($_ITEM->Description); ?> </div>
+<div class='days'>		<?php _p($_ITEM->Meetings); ?> </div>
+<div class='times'>			<?php _p($_ITEM->Times); ?> </div>
+<div class='type'>			<?php _p($_ITEM->StructuresHtml, false); ?> </div>	
+<div class='signup'>		<input type="button" value="Sign up" onclick="window.location='http://growthgroups.alcf.net/register.php';"> </div>
+<div style="clear:both;"></div>
+<hr>
 </div>
+
