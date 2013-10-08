20a94c62d535cb2c6a9fc7ce3c98df2d8e786a4e
Gareth Seeto <gsee@localhost.localdomain>
2013-10-08 16:32:48 -0700

    Updating growth group page for new look and feel

--- www/_growthgroups/index.tpl.php
+++ www/_growthgroups/index.tpl.php
@@ -38,19 +38,59 @@
 </script>
 <script type="text/javascript" src="/scripts/growthgroups.js"></script>
 
-<div id="map_canvas" style="width: 600px; height: 600px; border: 1px solid black; float: left;"></div>
- 
-<div style="float: left; margin-left: 15px; ">
+<div id="map_canvas" style="width: 980px; height: 400px; border: 1px solid black; float: left; margin-bottom:10px;"></div>
+
+	<h2 style="color:#fffac9; font-size:24px;">Growth Group Regions</h2>
+	<div style="background-color: #2F6C61; width: 980px; height: 72px; float:left; box-shadow: 10px 10px 5px #444444;">
+		<ul class="mapnav">
+		<?php
+			foreach (GrowthGroupLocation::LoadAll(QQ::OrderBy(QQN::GrowthGroupLocation()->Id)) as $objLocation) {
+				$strStyle = null;
+
+				if ($objLocation->Id == $this->objLocation->Id) {
+					$strLocation = QApplication::HtmlEntities($objLocation->Location);
+					$strLocation = str_replace(' (', '<br/><span style="font-size: 14px; font-weight: normal; font-family: arial;">(', $strLocation);
+					$strLocation = str_replace(')', ')</span>', $strLocation);
+					$strLocation = str_replace('including', 'incl.', $strLocation);
+					$strLocation = str_replace('to San', 'to<br/>San', $strLocation);
+					printf('<li class="selected"%s>%s</li>', $strStyle, $strLocation);
+				} else {
+					$strLocation = QApplication::HtmlEntities($objLocation->Location);
+					if (strpos($strLocation, '(')) {
+						$strLocation = str_replace(' (', '</a><br/><span style="font-size: 14px; font-weight: normal; font-family: arial; ">(', $strLocation);
+						$strLocation = str_replace(')', ')</span>', $strLocation);
+						$strLocation = str_replace('including', 'incl.', $strLocation);
+						printf('<li%s><a href="/index.php/%s">%s</li>', $strStyle, $objLocation->Id, $strLocation);
+					} else {
+						$strLocation = str_replace('to Mountain', 'to<br/>Mountain', $strLocation);
+						$strLocation = str_replace('to San', 'to<br/>San', $strLocation);
+						printf('<li%s><a href="/index.php/%s">%s</a></li>', $strStyle, $objLocation->Id, $strLocation);
+					}
+				}
+			}
+		?>
+		</ul>
+	</div> 
+	
+<div style="float: left; margin-top:20px; padding-top:10px; margin-bottom:20px; width:980px; ">
 	<div style="background-color: #2F6C61; padding: 10px; color: #fff; font-size: 12px; ">
 		<strong>Filter Results:</strong><br/>
 		<?php $this->lstDays->Render('Width=160px'); ?> &nbsp; <?php $this->lstTypes->Render('Width=160px'); ?>
 	</div>
 	<div style="background-color:#F5F8F7;">
+	<div style="color:#000000; font-weight:bold;">
+		<div class='name' >Group Name</div>
+		<div class='description'>Description</div>
+		<div class='days'>Meeting days</div>
+		<div class='times'>Times</div>
+		<div class='type' >Group Type</div>
+		<div class='signup'></div>
+	</div>
+	<div style="clear:both;"></div>
 	<?php $this->dtrGrowthGroups->Render(); ?>
 	<?php $this->pnlNone->Render(); ?>
 	</div>
 </div>
-
 <br clear="all"/><br/>
 
 <div style="margin: auto; text-align: center; font-size: 10px;">Developed by the <a href="http://www.alcf.net/it-team">ALCF Web Development Team</a></div>
