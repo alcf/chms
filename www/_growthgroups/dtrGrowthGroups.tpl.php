b64ee85fd386e250b13d5e98b0ca6bef4580f6d7
Gareth Seeto <gsee@localhost.localdomain>
2015-08-25 11:40:05 -0700

    Allowing html in growth group description

--- www/_growthgroups/dtrGrowthGroups.tpl.php
+++ www/_growthgroups/dtrGrowthGroups.tpl.php
@@ -3,7 +3,7 @@
 	<div class="marker"><a href="javascript:markerclick(<?php _p($this->intMarkerArray[$_ITEM->GroupId] - 1); ?>)" ><img src="/images/mapfiles/marker<?php _p($this->intMarkerArray[$_ITEM->GroupId]); ?>.png"/></a></div>
 	
 <strong><?php _p($_ITEM->Group->Name); ?></strong></div>
-<div class='description'>			<?php _p($_ITEM->Description); ?> </div>
+<div class='description'>			<?php _p($_ITEM->Description,false); ?> </div>
 <div class='days'>		<?php _p($_ITEM->Meetings); ?> </div>
 <div class='times'>			<?php _p($_ITEM->Times); ?> </div>
 <div class='type'>			<?php _p($_ITEM->StructuresHtml, false); ?> </div>	
